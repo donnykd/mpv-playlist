5d73c8129da805d9066b2d947ff4c0af6ead2925
khalid <ga.lid2002.km@gmail.com>
2025-08-31 01:14:53 +0100

    feat: playlist management functions

--- backend/playlist/playlist.go
+++ backend/playlist/playlist.go
@@ -3,6 +3,7 @@ package playlist
 import (
 	"fmt"
 	"os"
+	"slices"
 )
 
 type Playlist struct {
@@ -37,4 +38,30 @@ func (p *Playlist) GenerateM3uFile() (string, error) {
 	}
 
 	return tempFilePath, err
+}
+
+func (p *Playlist) Clear() {
+	p.paths = nil
+	p.currentFile = -1
+}
+
+func (p *Playlist) RemoveFile(index int) error {
+	if index < 0 || index > len(p.paths) {
+		return fmt.Errorf("index out range")
+	}
+
+	p.paths = slices.Delete(p.paths, index, index+1)
+	return nil
+}
+
+func (p *Playlist) IsEmpty() bool {
+	return len(p.paths) > 0
+}
+
+func (p *Playlist) Count() int {
+	return len(p.paths)
+}
+
+func (p *Playlist) GetFiles() []string {
+	return slices.Clone(p.paths)
 }
